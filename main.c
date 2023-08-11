be163083434ea1d6f19b2c38da64b001350fa2fe
Saskia Listle <slistle@icloud.com>
2023-08-11 18:26:59 +0200

    small progress

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: slistle <slistle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 22:08:32 by slistle           #+#    #+#             */
-/*   Updated: 2023/08/11 14:41:52 by slistle          ###   ########.fr       */
+/*   Updated: 2023/08/11 18:25:22 by slistle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ void ft_error(char *msg)
 	exit(1);
 }
 
-t_stack_list	*ft_last_elem(t_stack_list *lst)
+t_stack_list	*ft_find_last(t_stack_list *lst)
 {
+	if(!lst)
+		return(NULL);
 	while (lst)
 	{
 		if (lst->next == NULL)
@@ -29,57 +31,47 @@ t_stack_list	*ft_last_elem(t_stack_list *lst)
 	return (lst);
 }
 
-t_stack_list *ft_first_elem(t_stack_list *content, int nbr)
+t_stack_list	*ft_new_elem(int nbr)
 {
-	t_stack_list *lst;
+	t_stack_list	*lst;
 
-	lst->content = nbr;
-	lst->next = NULL;
-}
-
-void link_number(t_stack_list **stack, int nbr)
-{
-	t_stack_list *lst;
-	t_stack_list *last_elem;
-	
 	lst = malloc(sizeof(t_stack_list));
 	if (!lst)
-		return ;
-	if (*stack == NULL)
-		ft_first_elem(stack, nbr);
-	else
-	{	
-		last_elem = ft_last_elem(*stack);
-		last_elem->next = nbr;
-	}
-	
+		return (NULL);
+	lst->nbr = nbr;
+	lst->next = NULL;
+	return (lst);
 }
 
-void stack_init(t_stack_list **a, char **argv)
+void ft_add_back(t_stack_list **lst, t_stack_list *new)
 {
-	int	nbr;
-	int	i;
-
-	i = 0;
-	while(argv[i])
-	{
-		nbr = ft_atoi(argv[i]);
-		link_number(a, nbr);
-		ft_printf("[%d]\n", nbr);
-		
-		i++;
-	}
+	if (!(*lst))
+		*lst = new;
+	else
+		ft_find_last(*lst)->next = new;
 }
 
 int main(int argc, char **argv)
 {
 	t_stack_list	*a;
-	// t_stack_list	b;
+	// t_stack_list	*b;
+	t_stack_list	*new;
+	int				i;
+	int				nbr;
 	
 	a = NULL;
+	i = 0;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
 		argv = ft_split(argv[1], ' ');
-	stack_init(&a, argv);
+	while (argv[i] != NULL)
+	{
+		nbr = ft_atoi(argv[i]);
+		new = ft_new_elem(nbr);
+		ft_add_back(&a, new);
+		ft_printf("%d\n", new->nbr);
+		i++;
+	}
+	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: slistle <slistle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 22:22:44 by slistle           #+#    #+#             */
-/*   Updated: 2023/08/11 14:32:25 by slistle          ###   ########.fr       */
+/*   Updated: 2023/08/11 17:26:35 by slistle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,21 @@
 
 # include <stdlib.h>
 # include <limits.h>
-# include <stdbool.h>
 # include <unistd.h>
 # include "libft/libft.h"
 # include "ft_printf/ft_printf.h"
 
 typedef struct s_stack_list
 {
-	// int		value;
+	int		nbr;
 	// struct s_stack_list *target;
 	struct s_stack_list *next;
 	struct s_stack_list *content;
 }	t_stack_list;
 
-void stack_init(t_stack_list **a, char **argv);
-void link_number(t_stack_list **stack, int nbr);
-t_stack_list	*ft_last_elem(t_stack_list *lst);
+void ft_add_back(t_stack_list **lst, t_stack_list *new);
+t_stack_list	*ft_new_elem(int nbr);
+t_stack_list	*ft_find_last(t_stack_list *lst);
 void ft_error(char *msg);
 
 
